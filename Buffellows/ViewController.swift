eeb367d690cb64fe66e0cb80ea5847e6a7f4c90a
Ashish Chatterjee <ashishchatterjee@Ashishs-MacBook-Pro.local>
2017-07-21 21:57:12 -0700

    Initial Commit

---
+++ Buffellows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buffellows
+//
+//  Created by Ashish Chatterjee on 7/21/17.
+//  Copyright © 2017 Ashish Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
