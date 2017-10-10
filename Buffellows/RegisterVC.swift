5560b3c4b29d5a66c84cd25e7456bd3356d0eecc
Ashish Chatterjee <ashishchatterjee@Ashishs-MacBook-Pro.local>
2017-10-10 13:51:35 -0700

    first commit

---
+++ Buffellows/RegisterVC.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterVC.swift
+//  Buffellows
+//
+//  Created by Ashish Chatterjee on 10/10/17.
+//  Copyright © 2017 Ashish Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
