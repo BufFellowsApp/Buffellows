c9f1a9cf074563762d6b66506bfe18c405f6e9a1
Egg42 <ericgamgug@gmail.com>
2017-11-08 22:00:03 -0800

    Password Checking enabled

--- Buffellows/SettingsVC.swift
+++ Buffellows/SettingsVC.swift
@@ -7,15 +7,51 @@
 //
 
 import UIKit
+import Firebase
 
-class SettingsVC: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+class SettingsVC: UIViewController {
+
     
     
-
+    @IBOutlet weak var password1: UITextField!
+    @IBOutlet weak var password2: UITextField!
+    @IBOutlet weak var changePassword: UIButton!
+    @IBOutlet weak var pass2image: UIImageView!
+    @IBOutlet weak var pass1image: UIImageView!
+    
+    
+    @IBAction func changePassword(_ sender: Any) {
+   
+            
+                // [START change_password]
+            Auth.auth().currentUser?.updatePassword(to: password1.text!) { (error) in
+                    // [START_EXCLUDE]
+                
+                
+                    // [END_EXCLUDE]
+                }
+                // [END change_password]
+        
+    
+            print("Password Changed")
+            
+    
+            
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavBar()
+        
+        
+        password1.addTarget(self, action: #selector(textChanged(textField:)), for: .editingChanged)
+        
+        
+        password2.addTarget(self, action: #selector(textChanged(textField:)), for: .editingChanged)
+        
+        
+        
+        changePassword.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
@@ -26,6 +62,23 @@ class SettingsVC: UIViewController,  UITableViewDelegate, UITableViewDataSource 
     func handleSignout() {
         let loginVC = LoginVC(nibName: "LoginVC", bundle: nil)
         self.navigationController?.pushViewController(loginVC, animated: false)
+    }
+    func textChanged(textField: UITextField){
+        let t1pass = password1.text
+
+       
+        if (isPasswordValid(t1pass!)){
+            pass1image.image = UIImage(named: "exerciseSelected")
+            
+        }
+        
+        
+        if (password1.text == password2.text)  {
+            pass2image.image = UIImage(named: "exerciseSelected")
+            changePassword.isEnabled = true
+        } else {
+            changePassword.isEnabled = false
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -43,6 +96,10 @@ class SettingsVC: UIViewController,  UITableViewDelegate, UITableViewDataSource 
         // Pass the selected object to the new view controller.
     }
     */
+    func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{6,}")
+        return passwordTest.evaluate(with: password)
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
