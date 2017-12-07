ec79b1234e0780615863ce539241a49ae6d32eb9
Egg42 <ericgamgug@gmail.com>
2017-12-06 21:05:52 -0800

    friend list updated with custom cell

---
+++ Buffellows/FriendCell.swift
@@ -0,0 +1,18 @@
+//
+//  FriendCell.swift
+//  Buffellows
+//
+//  Created by Eric Gambetta-Guglielmana on 12/6/17.
+//  Copyright © 2017 Ashish Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var status: UILabel!
+    
+    
+}
