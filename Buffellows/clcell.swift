d91d43ce988e76e802a2564beb9ad8cbca5e251e
Egg42 <ericgamgug@gmail.com>
2017-12-12 18:56:17 -0800

    updates

---
+++ Buffellows/clcell.swift
@@ -0,0 +1,25 @@
+//
+//  clcell.swift
+//  Buffellows
+//
+//  Created by Eric Gambetta-Guglielmana on 12/10/17.
+//  Copyright © 2017 Ashish Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class clcell: UITableViewCell {
+
+    let cDB = challengeDB()
+    var challengeID = ""
+    var userID = ""
+    
+    @IBOutlet weak var challenge: UILabel!
+    @IBOutlet weak var complete: UIButton!
+    
+    @IBAction func cChallenge(_ sender: Any) {
+        cDB.completeTask(userID: userID, challengeID: challengeID)
+    }
+    
+    
+}
